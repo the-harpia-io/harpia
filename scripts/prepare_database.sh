3cf3ecf49f8ec7850bb504429605b845ccdcea83
nkondratyk <nkondratyk@playtika.com>
2022-09-04 22:20:56 +0300

    Initial commit

--- scripts/prepare_database.sh
+++ scripts/prepare_database.sh
@@ -1,4 +1,8 @@
+echo "Start preparing DB"
 mysql -h mariadb -u harpia -pharpia --execute="CREATE DATABASE IF NOT EXISTS harpia;"
+echo "DB was created"
 mysql -h mariadb -u harpia -pharpia --execute="set global tx_isolation='read-committed';"
 mysql -h mariadb -u harpia -pharpia --execute="CREATE USER IF NOT EXISTS 'harpia'@'%' IDENTIFIED BY 'harpia'; GRANT ALL PRIVILEGES ON *.* TO 'harpia'@'%';"
+echo "Start importing DB"
 mysql -h mariadb -u harpia -pharpia harpia < harp_db_template.sql
+echo "Finish importing DB"
