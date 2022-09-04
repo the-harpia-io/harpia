cc88f53646b7afceac616f53ad31d9ee316cf096
nkondratyk <nkondratyk@playtika.com>
2022-09-04 22:07:32 +0300

    Initial commit

--- scripts/prepare_database.sh
+++ scripts/prepare_database.sh
@@ -1,4 +1,4 @@
-mysql -u harpia -pharpia --execute="CREATE DATABASE IF NOT EXISTS harpia;" &&
-mysql -u harpia -pharpia --execute="set global tx_isolation='read-committed';" &&
+mysql -u harpia -pharpia --execute="CREATE DATABASE IF NOT EXISTS harpia;"
+mysql -u harpia -pharpia --execute="set global tx_isolation='read-committed';"
 mysql -u harpia -pharpia --execute="CREATE USER IF NOT EXISTS 'harpia'@'%' IDENTIFIED BY 'harpia'; GRANT ALL PRIVILEGES ON *.* TO 'harpia'@'%';"
-mysql -u harpia -pharpia harpia < ./harp_db_template.sql
+mysql -u harpia -pharpia harpia < harp_db_template.sql
