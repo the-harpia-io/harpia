10e83e556a7cf53719c9a97a6ee468e63fb11924
nkondratyk <nkondratyk@playtika.com>
2022-09-09 14:03:04 +0300

    Initial commit

--- scripts/prepare_database.sh
+++ scripts/prepare_database.sh
@@ -1,10 +1,14 @@
 echo "Wait for DB"
 sleep 10
 echo "Start preparing DB"
-mysql -h mariadb -u harpia -pharpia --execute="CREATE DATABASE IF NOT EXISTS harpia_db;"
-echo "DB was created"
-mysql -h mariadb -u harpia -pharpia --execute="set global tx_isolation='read-committed';"
-mysql -h mariadb -u harpia -pharpia --execute="CREATE USER IF NOT EXISTS 'harpia'@'%' IDENTIFIED BY 'harpia'; GRANT ALL PRIVILEGES ON *.* TO 'harpia'@'%';"
-echo "Start importing DB"
-mysql -h mariadb -u harpia -pharpia harpia_db < /tmp/harp_db_template.sql
-echo "Finish importing DB"
+
+RESULT=`mysqlshow --user=harpia --password=pharpia harpia_db| grep -v Wildcard | grep -o harpia_db`
+if [ "$RESULT" == "harpia_db" ]; then
+  mysql -h mariadb -u harpia -pharpia --execute="CREATE DATABASE IF NOT EXISTS harpia_db;"
+  echo "DB was created"
+  mysql -h mariadb -u harpia -pharpia --execute="set global tx_isolation='read-committed';"
+  mysql -h mariadb -u harpia -pharpia --execute="CREATE USER IF NOT EXISTS 'harpia'@'%' IDENTIFIED BY 'harpia'; GRANT ALL PRIVILEGES ON *.* TO 'harpia'@'%';"
+  echo "Start importing DB"
+  mysql -h mariadb -u harpia -pharpia harpia_db < /tmp/harp_db_template.sql
+  echo "Finish importing DB"
+fi
